0ef74b42f9cbc1541ec5a0aeb0ba9c4e52d118b5
forDecember <1921567337@qq.com>
2024-06-16 21:43:15 +0800

    update: 主函数和命令分离

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,109 +3,15 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-use std::fs;
-
-use serde::{Deserialize, Serialize};
-use serde_json::Number;
-use walkdir::WalkDir;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-fn upload_file(file: Vec<u8>, filename: &str) -> Result<String, String> {
-    use std::fs::File;
-    use std::io::Write;
-
-    let path = match filename.ends_with(".dll") {
-        true => format!("./bot-resources/wrapper/bin/{}", filename),
-        false => format!("./bot-resources/wrapper/config/{}", filename),
-    };
-
-    let mut output = File::create(path).map_err(|e| e.to_string())?;
-    output.write_all(&file).map_err(|e| e.to_string())?;
-
-    Ok("插件添加成功！".to_string())
-}
-
-#[tauri::command]
-fn upload_plugin(
-    json_file: Vec<u8>,
-    dll_file: Vec<u8>,
-    json_filename: String,
-) -> Result<String, String> {
-    // 同名上传
-    upload_file(json_file, &json_filename).and_then(|_| {
-        return upload_file(dll_file, &json_filename.replace(".json", ".dll"));
-    })
-}
-
-#[derive(Serialize, Deserialize)]
-struct PluginItem {
-    name: String,
-    privilege: Number,
-    filename: Option<String>, // 记录文件原始路径
-}
-
-#[tauri::command]
-fn get_plugins() -> Result<Vec<PluginItem>, String> {
-    let mut res: Vec<PluginItem> = Vec::new(); // 创建一个存放名字的向量
-    let path = "./bot-resources/wrapper/config/"; // 指定要遍历的目录
-
-    // 遍历目录和子目录中的所有文件
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        let path = entry.path();
-        // 检查文件是否是 JSON 文件
-        if path.extension().and_then(std::ffi::OsStr::to_str) == Some("json") {
-            // 读取文件内容
-            let data = fs::read_to_string(path).map_err(|e| e.to_string()).unwrap();
-            println!("{}", data);
-            // 解析 JSON 数据
-            let mut item: PluginItem = serde_json::from_str(&data.to_lowercase())
-                .map_err(|e| e.to_string())
-                .unwrap();
-
-            println!(
-                "{}",
-                path.file_name().and_then(std::ffi::OsStr::to_str).unwrap()
-            );
-            item.filename = Some(
-                path.file_name()
-                    .and_then(std::ffi::OsStr::to_str)
-                    .unwrap()
-                    .to_string(),
-            );
-            res.push(item);
-        }
-    }
-
-    Ok(res)
-}
-
-#[tauri::command]
-fn del_plugins(filename: String) -> String {
-    if let Err(e) = fs::remove_file(format!("./bot-resources/wrapper/config/{}", filename)) {
-        return String::from(e.to_string());
-    }
-
-    if let Err(e) = fs::remove_file(format!(
-        "./bot-resources/wrapper/bin/{}",
-        filename.replace(".json", ".dll")
-    )) {
-        String::from(e.to_string())
-    } else {
-        String::new()
-    }
-}
+mod command;
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            greet,
-            upload_plugin,
-            get_plugins,
-            del_plugins
+            command::greet,
+            command::upload_plugin,
+            command::get_plugins,
+            command::del_plugins
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
