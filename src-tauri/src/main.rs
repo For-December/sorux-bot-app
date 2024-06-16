5c8d3dfb180e1f13083814783b16591187df4a6d
forDecember <1921567337@qq.com>
2024-06-16 17:17:38 +0800

    update: 修改gitignore

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,23 +7,26 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-
 #[tauri::command]
 fn upload_file(file: Vec<u8>, filename: String) -> Result<String, String> {
     use std::fs::File;
     use std::io::Write;
+    
 
-    let path = format!("./bot-resources/wrapper/{}",filename);
+    let path = match filename.ends_with(".dll") {
+        true => format!("./bot-resources/wrapper/bin/{}", filename),
+        false => format!("./bot-resources/wrapper/config/{}", filename),
+    };
+
     let mut output = File::create(path).map_err(|e| e.to_string())?;
     output.write_all(&file).map_err(|e| e.to_string())?;
 
     Ok("文件上传成功".to_string())
 }
 
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet,upload_file])
+        .invoke_handler(tauri::generate_handler![greet, upload_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
