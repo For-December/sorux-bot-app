ccd4e567bc4b10d1dd4292d20a8ce1ce5e5d16fa
forDecember <1921567337@qq.com>
2024-06-17 13:51:01 +0800

    update: 成功实现退出后关闭开启的子进程

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,31 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-use std::process::exit;
+use std::{sync::mpsc, thread};
 
 mod command;
 mod global_constants;
 mod provider;
 
 fn main() {
-    provider::run_provider();
+    // 用来关闭子进程的通道
+    let (tx, rx) = mpsc::channel::<bool>();
+
+    let mut provider_child = provider::run_provider();
+    thread::spawn(move || {
+        // 在主线程中作为消费者接收并处理布尔值
+        for _ in rx {
+            println!("接收到退出信号！");
+            // 结束子进程
+            let _ = provider_child.kill().expect("Failed to kill child process");
+
+            // // 等待子进程结束
+            let _ = provider_child.wait().expect("Failed to wait on child");
+            break;
+        }
+        println!("监听线程退出！")
+    });
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             command::greet,
@@ -20,13 +37,16 @@ fn main() {
             command::watch_qrcode,
             command::init_process,
         ])
-        .on_window_event(|event| {
+        .on_window_event(move |event| {
             match event.event() {
-                tauri::WindowEvent::CloseRequested { api, .. } => {
-                    api.prevent_close();
+                tauri::WindowEvent::CloseRequested { .. } => {
+                    // api.prevent_close();
                     println!("窗口关闭！");
-                    let _ = event.window().close();
-                    exit(0);
+
+                    tx.send(true).unwrap();
+
+                    println!("程序结束！");
+                    // let _ = event.window().close();
                 }
                 _ => {} // TODO
             }
--- src-tauri/src/provider.rs
+++ src-tauri/src/provider.rs
@@ -1,19 +1,24 @@
 use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
+use std::process::{Child, Command, Stdio};
 use std::thread;
 
-pub fn run_provider() {
-    let _handler = thread::spawn(move || {
-        let mut child = Command::new("bot-resources/provider/SoruxBot.Provider.QQ.exe")
-            .current_dir("./bot-resources/provider")
-            .stdout(Stdio::piped()) // 将标准输出重定向到管道
-            .spawn() // 启动子进程
-            .expect("Failed to start C# program");
+use crate::global_constants::PROVIDER_DIR_PATH;
 
-        // 获取子进程的标准输出的句柄
-        if let Some(output) = child.stdout.take() {
-            let reader = BufReader::new(output);
+pub fn run_provider() -> Child {
+    // 创建子进程启动 provider
+    let mut child = Command::new(format!(
+        "{}/publish/SoruxBot.Provider.QQ.exe",
+        PROVIDER_DIR_PATH
+    ))
+    .current_dir(PROVIDER_DIR_PATH)
+    .stdout(Stdio::piped()) // 将标准输出重定向到管道
+    .spawn() // 启动子进程
+    .expect("Failed to start C# provider program");
 
+    // 获取子进程的标准输出的句柄，并打印输出
+    if let Some(output) = child.stdout.take() {
+        let reader = BufReader::new(output);
+        thread::spawn(move || {
             // 读取管道中的数据
             for line in reader.lines() {
                 match line {
@@ -21,10 +26,13 @@ pub fn run_provider() {
                     Err(e) => println!("Error reading line: {}", e),
                 }
             }
-        }
+        });
+    }
 
-        // child.
-        // 等待子进程结束
-        let _ = child.wait().expect("Failed to wait on child");
-    });
+    // child.
+    // 等待子进程结束
+    // let _ = child.wait().expect("Failed to wait on child")
+
+    // 返回子进程句柄，后续操作
+    child
 }
